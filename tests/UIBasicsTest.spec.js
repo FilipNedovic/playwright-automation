adbd4048162f2d20ea82f9612a71d296ec32dba0
FilipNedovic <filip.nedovic@automaticity.rs>
2024-01-08 16:33:13 +0100

    core concepts

---
+++ tests/UIBasicsTest.spec.js
@@ -0,0 +1,12 @@
+const { test, expect } = require("@playwright/test");
+
+test("Browser context Playwright test", async ({ browser }) => {
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise");
+});
+
+test("Page Playwright test", async ({ page }) => {
+  await page.goto("https://google.com");
+  await expect(page).toHaveTitle("Google");
+});
