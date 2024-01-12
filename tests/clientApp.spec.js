6aafab057c6714467257bbb0e918bca98af50f2b
FilipNedovic <filip.nedovic@automaticity.rs>
2024-01-12 09:26:50 +0100

    testing with multiple datasets

--- tests/clientApp.spec.js
+++ tests/clientApp.spec.js
@@ -3,61 +3,64 @@ const { POMManager } = require("../pom/pomManager");
 const { JSONUtils } = require("../utils/jsonUtils");
 
 const parser = new JSONUtils();
-const customer = parser.parseRequiredJSON("../utils/customerData.json");
+const customers = parser.parseRequiredJSON("../utils/customerData.json");
 const order = parser.parseRequiredJSON("../utils/orderData.json");
 
-test("ordering - happy flow", async ({ page }) => {
-  const pomManager = new POMManager(page);
+customers.forEach((customer, index) => {
+  test(`${index} ordering - happy flow`, async ({ page }) => {
+    const pomManager = new POMManager(page);
 
-  const [
-    loginPage,
-    dashboardPage,
-    cartPage,
-    checkoutPage,
-    navbar,
-    ordersHistoryPage,
-  ] = [
-    pomManager.getLoginPage(),
-    pomManager.getDashboardPage(),
-    pomManager.getCartPage(),
-    pomManager.getCheckoutPage(),
-    pomManager.getNavbar(),
-    pomManager.getOrderHistoryPage(),
-  ];
+    const [
+      loginPage,
+      dashboardPage,
+      cartPage,
+      checkoutPage,
+      navbar,
+      ordersHistoryPage,
+    ] = [
+      pomManager.getLoginPage(),
+      pomManager.getDashboardPage(),
+      pomManager.getCartPage(),
+      pomManager.getCheckoutPage(),
 
-  //  login
-  await loginPage.goToLoginPage();
-  await loginPage.validLogin(customer.credentials);
+      pomManager.getNavbar(),
+      pomManager.getOrderHistoryPage(),
+    ];
 
-  //  dashboard - add specific product to cart
-  await dashboardPage.addProductToCart(order.product.name);
+    //  login
+    await loginPage.goToLoginPage();
+    await loginPage.validLogin(customer.credentials);
 
-  //  cart - verify that product is added to cart
-  await navbar.navigateToCart();
-  const bool = page.locator("h3:has-text('Zara Coat 3')").isVisible();
-  expect(bool).toBeTruthy();
+    //  dashboard - add specific product to cart
+    await dashboardPage.addProductToCart(order.product.name);
 
-  //  checkout - verify email info
-  expect(checkoutPage.shippingInfoSection.locator("label").first()).toHaveText(
-    customer.credentials.email
-  );
+    //  cart - verify that product is added to cart
+    await navbar.navigateToCart();
+    const bool = page.locator("h3:has-text('Zara Coat 3')").isVisible();
+    expect(bool).toBeTruthy();
 
-  //  checkout - enter personal and shipping info
-  await cartPage.navigateToCheckout();
-  await checkoutPage.enterPersonalInfo(customer.personalInfo);
-  await checkoutPage.enterShippingInfo(customer.shippingInfo);
+    //  checkout - verify email info
+    expect(
+      checkoutPage.shippingInfoSection.locator("label").first()
+    ).toHaveText(customer.credentials.email);
 
-  //  place order
-  await checkoutPage.submitOrder();
-  await expect(page.locator("h1")).toHaveText(" Thankyou for the order. ");
+    //  checkout - enter personal and shipping info
+    await cartPage.navigateToCheckout();
+    await checkoutPage.enterPersonalInfo(customer.personalInfo);
+    await checkoutPage.enterShippingInfo(customer.shippingInfo);
 
-  // extract order id
-  let orderId = await page
-    .locator(".em-spacer-1 .ng-star-inserted")
-    .textContent();
-  orderId = orderId.split("|")[1].trim();
+    //  place order
+    await checkoutPage.submitOrder();
+    await expect(page.locator("h1")).toHaveText(" Thankyou for the order. ");
 
-  //  visit orders history and verify
-  await navbar.navigateToOrdersHistory();
-  await ordersHistoryPage.verifyPlacedOrder(orderId);
+    // extract order id
+    let orderId = await page
+      .locator(".em-spacer-1 .ng-star-inserted")
+      .textContent();
+    orderId = orderId.split("|")[1].trim();
+
+    //  visit orders history and verify
+    await navbar.navigateToOrdersHistory();
+    await ordersHistoryPage.verifyPlacedOrder(orderId);
+  });
 });
